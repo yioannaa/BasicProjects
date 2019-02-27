457b8656d8bec4bf143e8b2274a43d88afce056e
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-02-27 13:10:22 +0100

    randomPassword class added

--- src/main/java/pl/emailapp/Email.java
+++ src/main/java/pl/emailapp/Email.java
@@ -13,15 +13,23 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private String alternateEmail;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
+    private int defaultPasswordLength = 10;
+    private String companySuffix = "wow-company.com";
 
 
     public Email (String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
         this.department = setDepartment();
+        System.out.println("Your department is: " + this.department);
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
+
+
 
     private String setDepartment() {
         System.out.println("DEPARTMENT CODES:\n 1 for Sales \n 2 for Development \n 3 for Accouting \n" +
@@ -41,9 +49,43 @@ public class Email {
             case 0:
                 return " ";
         }
-
         return " ";
+    }
 
+    // Generate a random password
+    private String randomPassword (int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXZ0123456789!@#$%&*";
+        char [] password = new char [length];
+        for (int i = 0; i <length ; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+
+        return new String(password);
+    }
+
+    public void setMailboxCapacity (int capacity){
+        this.mailboxCapacity = capacity;
+    }
+
+    public void setAlternateEmail (String alternateEmail){
+        this.alternateEmail = alternateEmail;
+    }
+
+    public void changePassword (String password){
+        this.password = password;
+    }
+
+    public int getMailboxCapacity(){
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail(){
+        return alternateEmail;
+    }
+
+    public String getPassword(){
+        return password;
     }
 
 
