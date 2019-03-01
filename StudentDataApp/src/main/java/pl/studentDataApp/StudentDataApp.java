6411e9717002d70c10d316f63b9a054d446427be
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-03-01 15:45:23 +0100

    array of students

--- StudentDataApp/src/main/java/pl/studentDataApp/StudentDataApp.java
+++ StudentDataApp/src/main/java/pl/studentDataApp/StudentDataApp.java
@@ -1,13 +1,28 @@
 package pl.studentDataApp;
 
+import java.util.Scanner;
+
 public class StudentDataApp {
 
     public static void main(String[] args) {
 
-        Student student = new Student();
-        student.enroll();
-        student.payTuition();
-        System.out.println(student.showInfo());
+        System.out.println("Enter number of students you want to enroll");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStudents = scanner.nextInt();
+        Student []students = new Student[numberOfStudents];
+
+        for (int i = 0; i < numberOfStudents; i++) {
+
+            students [i]  = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+
+        }
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println(students[i].showInfo());
+
+        }
 
 
 
