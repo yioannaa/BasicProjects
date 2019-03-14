9390db58091fbac2ea2bee699f7a22bb5134db00
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-03-14 19:28:24 +0100

    list of objects in main added

--- BankAccountApp/src/main/java/pl/bankaccountapp/BankAccountApp.java
+++ BankAccountApp/src/main/java/pl/bankaccountapp/BankAccountApp.java
@@ -2,6 +2,7 @@ package pl.bankaccountapp;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
@@ -28,18 +29,27 @@ public class BankAccountApp {
 //        savacc1.compound();
 
 
+        List<Account>accounts = new LinkedList<Account>();
         List<String[]> newAccountHolders = Reader.read(file);
         for (String[] accountHolder : newAccountHolders) {
-            System.out.println("NEW ACCOUNT");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+            String name = accountHolder[0];
+            String socialSecurityNumber = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+
+            if (accountType.equals("Savings")){
+                accounts.add(new Saving(name, socialSecurityNumber, initDeposit));
+            }else if (accountType.equals("Checking")){
+                accounts.add(new Checking(name, socialSecurityNumber, initDeposit));
+            }else{
+                System.out.println("ERROR");
+            }
         }
 
-
-
-
+        for (Account acc : accounts){
+            System.out.println("\n**************************");
+            acc.showInfo();
+        }
 
     }
 }
